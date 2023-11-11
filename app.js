d316e264259a6adc0732a78234c3486597676199
AnKlvy <andreyklimov.ver2@gmail.com>
2023-11-11 20:14:55 +0600

    настроил шаблон jade

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexapp = require('./routes/index');
+var usersapp = require('./routes/users');
 
-const { MongoClient } = require("mongodb");
-const cors = require("cors");
-const { ObjectId } = require("mongodb"); // Импортируйте ObjectId из mongodb
+// const { MongoClient } = require("mongodb");
+// const cors = require("cors");
+// const { ObjectId } = require("mongodb"); // Импортируйте ObjectId из mongodb
 
 const uri = "mongodb://127.0.0.1:27017/";
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 let students; // Создаем переменную для коллекции студентов
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexapp);
+app.use('/users', usersapp);
 
-app.use(cors());
-app.use(Express.json()); //для разбора JSON-тела запросов
+// app.use(cors());
+// app.use(Express.json()); //для разбора JSON-тела запросов
 
 
 // catch 404 and forward to error handler
@@ -53,96 +54,18 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 
-async function connectToMongoDB() {
-  try {
-    await client.connect();
-    const database = client.db("university");
-    students = database.collection("students");
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-  }
-}
+// async function connectToMongoDB() {
+//   try {
+//     await client.connect();
+//     const database = client.db("university");
+//     students = database.collection("students");
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("MongoDB connection failed:", error);
+//   }
+// }
 
 app.listen(8080, () => {
-//   console.log("Server is running on port 5039");
-  connectToMongoDB();
-});
-
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  //   console.log("Server is running on port 5039");
+    // connectToMongoDB();
   });
-
-
-// Read collection
-app.get("/api/university/getStudents", async (request, response) => {
-  try {
-    const result = await students.find({}).toArray();
-    response.send(result);
-  } catch (error) {
-    console.error("Error fetching students:", error);
-    response.status(500).send("Internal Server Error");
-  }
-});
-
-// Add a new document to the collection
-app.post("/api/university/addStudent", async (req, res) => {
-    // try{
-    // let newDocument = req.body;
-    // console.log(newDocument)
-    const result = await students.insertOne(req.body);
-    res.send(result);
-    // }
-    // catch(error){
-    //     console.error("Error fetching students:", error);
-    // }
-});
-
-
-// Get a single post
-app.get("/api/university/getStudent/:_id", async (req, res) => {
-  let query = { _id: new ObjectId(req.params._id) };
-  let result = await students.findOne(query);
-  if (!result) {
-    res.status(404).json({ error: "Not found" });
-  } else {
-    res.status(200).json(result);
-  }
-});
-
-app.put("/api/university/updateStudent/:_id", async (req, res) => {
-  const query = { _id: new ObjectId(req.params._id) };
-  console.log(req.params._id)
-  const updates = {
-    $set: {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password }
-  };
-  console.log(req.body)
-  try{
-    let result = await students.updateOne(query, updates);
-    console.log(result)
-    res.send(result).status(200);
-  }
-  catch(e){
-    console.log(e)
-  }
-
-});
-
-// Удаление студента по идентификатору
-app.delete("/api/university/deleteStudent/:_id", async (req, res) => {
-  const query = { _id: new ObjectId(req.params._id) };
-  try {
-    let result = await students.deleteOne(query);
-    if (result.deletedCount > 0) {
-      res.status(200).send({});
-    } else {
-      res.status(404).send("Student not found");
-    }
-  } catch (error) {
-    console.error("Error deleting student:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,97 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
+
+
+  
+  router.get("/", (req, res) => {
+      const data = {
+        title: 'Пример страницы с jade',
+        message: 'Привет, мир! Это пример использования jade в Express.',
+    };
+  
+    // Отображение страницы index.ejs и передача данных
+      res.render('index', { data:data });
+      // res.send("Hello, World!");
+  
+  });
+  
+  
+  // Read collection
+  router.get("/api/university/getStudents", async (request, response) => {
+    try {
+      const result = await students.find({}).toArray();
+      response.send(result);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+      response.status(500).send("Internal Server Error");
+    }
+  });
+  
+  // Add a new document to the collection
+  router.post("/api/university/addStudent", async (req, res) => {
+      // try{
+      // let newDocument = req.body;
+      // console.log(newDocument)
+      const result = await students.insertOne(req.body);
+      res.send(result);
+      // }
+      // catch(error){
+      //     console.error("Error fetching students:", error);
+      // }
+  });
+  
+  
+  // Get a single post
+  router.get("/api/university/getStudent/:_id", async (req, res) => {
+    let query = { _id: new ObjectId(req.params._id) };
+    let result = await students.findOne(query);
+    if (!result) {
+      res.status(404).json({ error: "Not found" });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+  
+  router.put("/api/university/updateStudent/:_id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params._id) };
+    console.log(req.params._id)
+    const updates = {
+      $set: {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password }
+    };
+    console.log(req.body)
+    try{
+      let result = await students.updateOne(query, updates);
+      console.log(result)
+      res.send(result).status(200);
+    }
+    catch(e){
+      console.log(e)
+    }
+  
+  });
+  
+  // Удаление студента по идентификатору
+  router.delete("/api/university/deleteStudent/:_id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params._id) };
+    try {
+      let result = await students.deleteOne(query);
+      if (result.deletedCount > 0) {
+        res.status(200).send({});
+      } else {
+        res.status(404).send("Student not found");
+      }
+    } catch (error) {
+      console.error("Error deleting student:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
